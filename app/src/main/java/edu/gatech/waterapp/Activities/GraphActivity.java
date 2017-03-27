9e9e4f1a1a2ba4836787edcb455d61a93efda13b
Kevin Lieu <klieu6@gatech.edu>
2017-03-27 19:25:57 -0400

    fixed the GraphActivity

--- app/src/main/java/edu/gatech/waterapp/Activities/GraphActivity.java
+++ app/src/main/java/edu/gatech/waterapp/Activities/GraphActivity.java
@@ -1,5 +1,15 @@
 package edu.gatech.waterapp.Activities;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
-public class GraphActivity {
+import edu.gatech.waterapp.R;
+
+public class GraphActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graph);
+    }
 }
