58b57e467420deb1095c10cd8c658a12b5dec52b
coreycaskey <ccaskey6@gatech.edu>
2017-04-12 13:00:14 -0400

    Remove unnecessary comments from JUnits

--- app/src/androidTest/java/edu/gatech/waterapp/CheckPurityFieldsTest.java
+++ app/src/androidTest/java/edu/gatech/waterapp/CheckPurityFieldsTest.java
@@ -12,20 +12,8 @@ import org.junit.runner.RunWith;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import junit.framework.Assert;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-
-import android.widget.TextView;
 
 import edu.gatech.waterapp.Activities.PurityActivity;
 
